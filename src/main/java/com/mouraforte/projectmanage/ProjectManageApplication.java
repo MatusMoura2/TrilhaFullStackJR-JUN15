ab959037347576f110290b7fce9afc5b7318aac6
Matus Moura <matus.moura333x2@gmail.com>
2024-07-10 18:58:40 -0300

    primeiro commit

---
+++ src/main/java/com/mouraforte/projectmanage/ProjectManageApplication.java
@@ -0,0 +1,13 @@
+package com.mouraforte.projectmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManageApplication.class, args);
+	}
+
+}
